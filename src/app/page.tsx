dffe77a2105a4a0072054f06efb03ff0627cc836
Ian Hart <ian@ianmadethis.com>
2023-10-02 17:47:53 +0100

    Update comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,23 @@ import BookList from '../components/bookList'
 const Home = () => {
   const [getBookData, bookData, errorMessage] = useOpenLibraryAPI()
 
-  // TODO: Optimise with useMemo and useCallback.
+  // Flags and functions for Search Context.
   // NOTE: This is where we could set the default value to be derived from somewhere else (e.g Redux, slug, querystring etc).
   const [searchTerm, setSearchTerm] = useState<string>('')
   const [isSearching, setIsSearching] = useState<boolean>(false)
-
-  const [books, setBooks] = useState<Books>([])
-  const [hasSearched, setHasSearched] = useState<boolean>(false)
-
   const performSearch = () => {
     setIsSearching(true)
     setHasSearched(true)
     getBookData(searchTerm)
   }
 
+  // This flag is used to define the app's initial state.
+  const [hasSearched, setHasSearched] = useState<boolean>(false)
+  
+  // The core search result data.
+  const [books, setBooks] = useState<Books>([])
+
+  // The Book List UI is updated when the search API responds.
   useEffect(() => {
     setIsSearching(false)
     setBooks(getBooksFromData(bookData))
