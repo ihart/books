f32a306a199538e78e03a69eba94671f88c9e975
Ian Hart <ian@ianmadethis.com>
2023-10-03 16:04:59 +0100

    Add comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,24 @@ import Search from '../components/search'
 import BookList from '../components/bookList'
 
 const App = () => {
+  // Our core book list data.
+  const [books, setBooks] = useState<Books>([])
+
+  // Use a custom hook for calling the API and reading its bookData value.
   const [getBookData, bookData, errorMessage] = useOpenLibraryAPI()
 
-  // Flags and functions for Search Context.
-  // NOTE: This is where we could set the default value to be derived from somewhere else (e.g Redux, slug, querystring etc).
+  // Respond to changes of bookData to set our local state books.
+  useEffect(() => {
+    setIsSearching(false)
+    setBooks(getBooksFromData(bookData))
+  }, [bookData])
+
+  // State and functions for Search Context.
+  // This is where we could set the default value to be derived from 
+  // somewhere else (like a slug/querystring) and have useEffect react.
+  // We also add state management (e.g Redux) for storing history, caching etc.
   const [searchTerm, setSearchTerm] = useState<string>('')
-  const [isSearching, setIsSearching] = useState<boolean>(false)
+  const [isSearching, setIsSearching] = useState<boolean>(false) // This should probably be part of the API Hook.
   const performSearch = () => {
     setIsSearching(true)
     setHasSearched(true)
@@ -23,16 +35,8 @@ const App = () => {
   }
 
   // This flag is used to define the app's initial state.
+  // Tbh it would be best to break this out into a UI state to tidy up the JSX below.
   const [hasSearched, setHasSearched] = useState<boolean>(false)
-  
-  // The core search result data.
-  const [books, setBooks] = useState<Books>([])
-
-  // The Book List UI is updated when the search API responds.
-  useEffect(() => {
-    setIsSearching(false)
-    setBooks(getBooksFromData(bookData))
-  }, [bookData])
 
   return <main className={'container mx-auto max-w-3xl py-24'}>
     <h1 className={'text-5xl text-center pb-12'}>Books!</h1>
